5ea32cc5c62d113831d76000452c0385a0faa030
Savino Rasola <savinorasola@Savinos-MBP.home>
2019-12-22 16:59:29 +0100

    added JS execize + last part of the frontend + first part of PHP

--- index.php
+++ index.php
@@ -16,14 +16,13 @@
   </div>
 
   <div class="main-container">
+
     <div class="container">
       <div class="are-you-the-developer-we-are-l">
         <h1 class="are-you-the-develope">Are you the developer we’re looking for?</h1>
         <p class="nam-porttitor-blandi">Nam porttitor blandit accumsan. Ut vel dictum sem, a pretium dui. In malesuada enim in dolor euismod, id commodo mi consectetur. Curabitur at vestibulum nisi. Nullam vehicula nisi velit. Mauris turpis nisl, molestie ut ipsum et, suscipit vehicula odio. Vestibulum interdum vestibulum felis ac molestie.</p>
         <div class="container">
-          <div class="rectangle">
-            <div class="button">Button</div>
-          </div>
+          <button>Button</button>
         </div>
       </div>
     </div>
@@ -82,15 +81,101 @@
     <!-- end column system -->
 
     <div class="content-generator">
-      <h1>Content generator</h1>
-      <div class="code">
+      <h2>Content generator</h2>
+      <div class="code"></div>
+    </div>
+    
+    <div class="one-full vacancies">
+      <h2>Vacancies</h2>
+      <p class="far-far-away-behind">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
 
+      <div class="vacancies-container">
+        <div class="vacancies-card">
+          <h2>Junior Frontend Developer</h2>
+          <p class="subtitle">
+            Amsterdam - Full-time
+          </p>
+        </div>
+        <div class="vacancies-card">
+          <h2>Frontend Developer</h2>
+          <p class="subtitle">
+            Amsterdam - Part-time
+          </p>
+        </div>
+        <div class="vacancies-card">
+          <h2>Senior Frontend Developer</h2>
+          <p class="subtitle">
+            Amsterdam - Full-time
+          </p>
+        </div>
       </div>
+    </div>
+
+    <div class="three-big-icons images">
+      <img src="./assets/resources/squared/squared-image-1.jpg" alt="">
+      <img src="./assets/resources/squared/squared-image-2.jpg" alt="">
+      <img src="./assets/resources/squared/squared-image-3.jpg" alt="">
+    </div>
+
+    <div class="one-full centered">
+      <h2>Creative space</h2>
+      <p class="far-far-away-behind">Nam porttitor blandit accumsan. Ut vel dictum sem, a pretium dui. In malesuada enim in dolor euismod, id commodo mi consectetur. Curabitur at vestibulum nisi. </p>
     </div>
 
   </div>
   <!-- end main container -->
-  <footer></footer>
+  <footer>
+    <div class="footer-container">
+      <div class="icons-container">
+        <a href="#"><div class="facebook icons footer-icons"></div></a>
+        <a href="#"><div class="instagram icons footer-icons"></div></a>
+        <a href="#"><div class="linkedin icons footer-icons"></div></a>
+      </div>
+      <p>Copyright Site.nu 2019</p>  
+    </div>
+  </footer>
+
+  <?php
+    function callAPI($method, $url, $data) {
+      $curl = curl_init();
+  
+      switch ($method){
+        case "POST":
+            // Set the CURLOPT_POST option to true for POST request
+            curl_setopt($curl, CURLOPT_POST, true);
+            if ($data)
+              // Set the request data as JSON using json_encode function
+              curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        default:
+            if ($data)
+              $url = sprintf("%s?%s", $url, http_build_query($data));
+      }
+  
+      // OPTIONS:
+      curl_setopt($curl, CURLOPT_URL, $url);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  
+      // EXECUTE:
+      $result = curl_exec($curl);
+      if(!$result){die("Connection Failure");}
+      curl_close($curl);
+      return $result;
+    }
+    // GET REQUEST
+    $getCall = callAPI('GET', 'https://site.nu/interview-endpoint.php', false);
+    
+    // part 2.
+    $data_array =  array(
+      "username" => "sitenu",
+      "password" => "weareawesome",
+    );
+
+    $makeCall = callAPI('POST', 'https://site.nu/interview-endpoint.php', json_encode($data_array));
+
+    // I'm a bit stuck in this second part I need to study a bit more about PHP 
+  ?>
 
   <script src="./assets/javascript/main.js"></script>
 </body>
